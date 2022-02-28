705544b4ee953174a3d2e87619e037b42f938d33
Arthur231456 <artur.231456@gmail.com>
2022-03-01 00:19:57 +0300

    search bug fixed

--- data/dialog_argpars.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import reqparse
-
-parser = reqparse.RequestParser()
-parser.add_argument('file', required=True)
-parser.add_argument('members', required=True)
-parser.add_argument('id', required=True, type=int)
--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from data.products import Product
 from data.recipes import Recipe
 from flask_restful import reqparse, abort, Api, Resource
 
-
 # Инициализация приложения
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "QWav-43sd-svs3-001a"
@@ -20,13 +19,13 @@ DAILY_RECIPES = None
 CATEGORY = "all"
 string_id = None
 
-
 # API приложения
 api = Api(app)
 api.add_resource(recipe_resource.RecipeListResource, "/api/fridge-x-lite-api/recipes")
 api.add_resource(recipe_resource.RecipeResource, "/api/fridge-x-lite-api/recipe/<int:user_id>")
 api.add_resource(recipe_resource.RecipeDailyResource, "/api/fridge-x-lite-api/recipes/daily")
 api.add_resource(recipe_resource.RecipeCategoryResource, "/api/fridge-x-lite-api/recipes/category/<string:category>")
+
 
 # функция запуска приложения
 def main():
@@ -43,14 +42,8 @@ def main():
     app.run(host="0.0.0.0", port=port)
 
 
-# главная страница социальной сети
-@app.route("/", methods=["GET"])
-def base():
-    return render_template("base.html", title="FridgeXWeb")
-
-
 # функция для страницы рецептов
-@app.route("/fridge", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def catalog():
     global DAILY_RECIPES, db_sess, string_id, CATEGORY
     try:
@@ -71,19 +64,22 @@ def catalog():
             except:
                 pass
             if CATEGORY != "all":
-                recipes = db_sess.query(Recipe).filter(and_(Recipe.proteins <= protein,
-                                                       Recipe.carboh <= carboh,
-                                                       Recipe.fats <= fats,
-                                                       Recipe.time <= time + 10,
-                                                       Recipe.category_global == CATEGORY)).all()
+                recipes = db_sess.query(Recipe).filter(and_(Recipe.recipe_name.like(f"%{title}%"),
+                                                            Recipe.proteins <= protein,
+                                                            Recipe.carboh <= carboh,
+                                                            Recipe.fats <= fats,
+                                                            Recipe.time <= time + 10,
+                                                            Recipe.category_global == CATEGORY)).all()
                 print("yryt")
             else:
-                recipes = db_sess.query(Recipe).filter(and_(Recipe.proteins <= protein,
-                                                       Recipe.carboh <= carboh,
-                                                       Recipe.fats <= fats,
-                                                       Recipe.time <= time + 10)
+                recipes = db_sess.query(Recipe).filter(and_(Recipe.recipe_name.like(f"%{title}%"),
+                                                            Recipe.proteins <= protein,
+                                                            Recipe.carboh <= carboh,
+                                                            Recipe.fats <= fats,
+                                                            Recipe.time <= time + 10)
                                                        ).all()
-        return render_template("catalog.html", recipes=recipes, form=form, search_recipe=search_recipe, daily_recipes=DAILY_RECIPES, string_id=string_id)
+        return render_template("catalog.html", recipes=recipes, form=form, search_recipe=search_recipe,
+                               daily_recipes=DAILY_RECIPES, string_id=string_id)
     except Exception as e:
         return redirect(f"fridge/error/{e}")
 
@@ -96,13 +92,17 @@ def recipe(id: int):
     ingr = recipe.recipe.split()
     ingredients = list(map(lambda x: db_sess.query(Product).filter(Product.id == int(x)).first().product, ingr))
     value = recipe.recipe_value.split()
+    for i in range(len(value)):
+        if value[i] == "-1":
+            value[i] = "по вкусу"
     prepare = recipe.actions.split("\n")
     return render_template("recipe.html",
                            recipe=recipe,
                            ingredients=ingredients,
                            prepare=prepare,
                            id=str(recipe.id),
-                           value=value)
+                           value=value,
+                           count=len(value))
 
 
 @app.route("/fridge/error/<message>", methods=["POST", "GET"])
